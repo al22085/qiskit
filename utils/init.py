a7dd8e8ed5f9d85161c34a74db9ef31f4dc73185
al22085 <167169907+al22085@users.noreply.github.com>
2025-04-27 19:52:50 +0900

    [add] samples_2

--- utils/init.py
+++ utils/init.py
@@ -1,8 +1,9 @@
 # First, import all the necessary python modules
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython.display import Math
 from scipy.optimize import minimize, Bounds
-from qiskit import QuantumCircuit, transpile
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit.visualization import plot_distribution
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as IBMSampler
 from qiskit_ibm_runtime.accounts import AccountNotFoundError
@@ -11,7 +12,7 @@ from qiskit_aer.primitives import SamplerV2 as LocalSampler
 # qc_workbook is the original module written for this workbook
 # If you encounter an ImportError, edit the environment variable PYTHONPATH or sys.path
 from external.qc_workbook.source.qc_workbook.utils import operational_backend
-print('notebook ready')
+from external.qc_workbook.source.qc_workbook.show_state import statevector_expr
 
 from utils.init_token import service
 print('token ready')
