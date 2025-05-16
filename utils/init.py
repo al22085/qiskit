e057c9907cee03f6dfdb79f991eaa90059480c93
al22085 <167169907+al22085@users.noreply.github.com>
2025-05-16 10:19:15 +0900

    [add] samples_3

--- utils/init.py
+++ utils/init.py
@@ -11,8 +11,11 @@ from qiskit_aer import AerSimulator
 from qiskit_aer.primitives import SamplerV2 as LocalSampler
 # qc_workbook is the original module written for this workbook
 # If you encounter an ImportError, edit the environment variable PYTHONPATH or sys.path
-from external.qc_workbook.source.qc_workbook.utils import operational_backend
+from external.qc_workbook.source.qc_workbook.utils import operational_backend, find_best_chain
 from external.qc_workbook.source.qc_workbook.show_state import statevector_expr
+from external.qc_workbook.source.qc_workbook.optimized_additions import optimized_additions
+
+print('notebook ready')
 
 from utils.init_token import service
 print('token ready')
